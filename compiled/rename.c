b950c94bdb3a31b6afe7987a76fe1fd0fa55f65c
landcaster <landcaster@gmail.com>
2015-03-20 12:10:53 -1000

    Quick and dirty code to rename our fastq sequences

---
+++ compiled/rename.c
@@ -0,0 +1,34 @@
+#include <zlib.h>
+#include <stdio.h>
+#include <inttypes.h>
+#include "kseq.h"
+
+KSEQ_INIT(gzFile, gzread)
+
+
+/*https://github.com/lh3/readfq*/
+int main(int argc, char * argv[]){
+  if(argc < 2 || argc > 3){
+    fprintf(stderr, "USAGE: %s <fastq file> [prefix]\n",argv[0]);
+    return 0;
+  }
+  gzFile fp;
+  kseq_t *seq;
+  fp = gzopen(argv[1], "r");
+  seq = kseq_init(fp);
+  size_t idx = 0;
+  if(argc == 3){
+    while (kseq_read(seq) >= 0){
+      fprintf(stdout, "@%s_%"PRId64"\n%s\n+%s_%"PRId64"\n%s\n", argv[2], idx, seq->seq.s, argv[2] , idx, seq->qual.s);
+      ++ idx;
+    }
+  } else {
+    while (kseq_read(seq) >= 0){
+      fprintf(stdout, "@%"PRId64"\n%s\n+%"PRId64"\n%s\n", idx, seq->seq.s, idx, seq->qual.s);
+      ++ idx;
+    }
+  } 
+  kseq_destroy(seq);
+  gzclose(fp);
+  return 0;
+}
