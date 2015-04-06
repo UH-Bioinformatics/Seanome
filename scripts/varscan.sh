c2dec4fc66acb9bc45958a48eb389cb1babdf9cd
David S <davidls@poire.(none)>
2015-04-05 14:22:49 -1000

    added a way to hopefully auto locate the varscan.jar.. as long as it is on the users PATH

--- scripts/varscan.sh
+++ scripts/varscan.sh
@@ -4,7 +4,8 @@
 : ${2?"Usage: $0 ids_lst bamsuffix reference"}
 : ${3?"Usage: $0 ids_lst bamsuffix reference"}
 
-samtools mpileup -f ${3}  -b ${1} | java -jar /home/davidls/dedup_testing/Seanome/scripts/VarScan.jar mpileup2snp --min-var-freq 0.01 --output-vcf 1   2> /dev/null
+vscan=`which VarScan.jar`
+samtools mpileup -f ${3}  -b ${1} | java -jar ${vscan} mpileup2snp --min-var-freq 0.01 --output-vcf 1   2> /dev/null
 
 rm *_${2} 2> /dev/null
 rm ${1} 2> /dev/null
