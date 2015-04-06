6341b0246d98264617c537d04ae38181e94e1906
davidls <davidls@zoanthus.(none)>
2015-04-06 09:27:43 -1000

    removed a few unused scripts

--- scripts/renamer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-from Bio import SeqIO
-import sys
-
-with open(sys.argv[3], "w") as o:
-    for s in SeqIO.parse(sys.argv[1], "fastq"):
-        s.id = "%s_%s"%(sys.argv[2], s.id)
-        SeqIO.write([s], o, "fastq")
--- scripts/sam_to_bam.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-samfile=${1}
-bamfile=${1%.sam}
-
-samtools view -bS ${samfile} | samtools sort - ${bamfile}
-samtools index ${bamfile}.bam
