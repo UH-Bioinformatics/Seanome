d365903ca4b0bcf653308eff81d0d9da20f9bb47
davidls <davidls@zoanthus.(none)>
2015-04-15 10:47:46 -1000

    update to note in varscan.sh

--- scripts/varscan.sh
+++ scripts/varscan.sh
@@ -4,7 +4,7 @@
 : ${2?"Usage: $0 ids_lst bamsuffix reference"}
 : ${3?"Usage: $0 ids_lst bamsuffix reference"}
 
-# we are assuming this jar file is on the user path
+# we are assuming this jar file is on the user path and that it is marked as executable
 vscan=`which VarScan.jar`
 samtools mpileup -f ${3}  -b ${1} | java -jar ${vscan} mpileup2snp --min-var-freq 0.01 --output-vcf 1   2> /dev/null
 
