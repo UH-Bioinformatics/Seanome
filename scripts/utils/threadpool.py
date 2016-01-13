262e7c585c0643aa84bf0732a0331ef3e971c797
landcaster <landcaster@gmail.com>
2016-01-12 21:45:10 -1000

    Added a debug flag, so that we can pass it along to the threading code, to force it to run without pooling.

--- scripts/utils/threadpool.py
+++ scripts/utils/threadpool.py
@@ -4,17 +4,22 @@ from multiprocessing import Pool
 class ProducerConsumer(object):
     def __init__(self, args, threads, producer, consumer):
         # we subtract 1 thread, to account for the consumer thread. aka the main thread
-        if threads > 1:
-            self.threads= threads - 1
-        else:
-            self.threads = 1
+		self.debug = args.debug
+		if threads > 1:
+			self.threads= threads - 1
+		else:
+			self.threads = 1
         self.producer = producer
         self.consumer = consumer
 
     def run(self, passthrough, producerargs):
-        pool = Pool(processes = self.threads)
-        ret = pool.imap_unordered(self.producer, producerargs)
-        self.consumer(passthrough, ret)
-        pool.close()
+		if not self.debug:
+			pool = Pool(processes = self.threads)
+			ret = pool.imap_unordered(self.producer, producerargs)
+			self.consumer(passthrough, ret)
+			pool.close()
+		else:
+			self.consumer(passthrough, ( self.producer(d) for d in producerargs ) )
+				
 
         
